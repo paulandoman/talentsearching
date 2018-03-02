f34bd00bcaefde9afa6b5e98589bba4fbcae0173
Paul Anderson <panderson@seek.com.au>
2018-03-03 09:55:21 +1100

    modified comments

--- utils_test.go
+++ utils_test.go
@@ -9,21 +9,21 @@ func TestTruncate(t *testing.T) {
 	expected := 197.90
 	truncVal := Truncate(val)
 	if truncVal != expected {
-		t.Errorf("Didn't round down value succesfully, got: %v, expected: %v", truncVal, expected)
+		t.Errorf("Truncated value unsuccessfully, got: %v, expected: %v", truncVal, expected)
 	}
 
 	val = 23.255
 	expected = 23.25
 	truncVal = Truncate(val)
 	if truncVal != expected {
-		t.Errorf("Didn't round down value succesfully, got: %v, expected: %v", truncVal, expected)
+		t.Errorf("Truncated value unsuccessfully, got: %v, expected: %v", truncVal, expected)
 	}
 
 	val = 10.0099
 	expected = 10.00
 	truncVal = Truncate(val)
 	if truncVal != expected {
-		t.Errorf("Didn't round down value succesfully, got: %v, expected: %v", truncVal, expected)
+		t.Errorf("Truncated value unsuccessfully, got: %v, expected: %v", truncVal, expected)
 	}
 
 }
